1ad4bb04a48e579e9ed7f7708b424b31a4ef1579
frank <franktranvantu@gmail.com>
2021-08-19 22:58:30 +0700

    mastering-spring-data-jpa-1 Setup project

---
+++ src/test/java/com/franktran/masteringspringdatajpa/MasteringSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franktran.masteringspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpringDataJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
