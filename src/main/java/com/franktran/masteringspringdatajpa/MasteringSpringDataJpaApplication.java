2c779641ed83c3ec43bbfa32300394a818e8de3f
frank <franktranvantu@gmail.com>
2021-08-20 22:25:18 +0700

    mastering-spring-data-jpa-20 Exploring Repository Methods

--- src/main/java/com/franktran/masteringspringdatajpa/MasteringSpringDataJpaApplication.java
+++ src/main/java/com/franktran/masteringspringdatajpa/MasteringSpringDataJpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MasteringSpringDataJpaApplication {
 
@@ -16,7 +18,26 @@ public class MasteringSpringDataJpaApplication {
   public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
     return args -> {
       Student frank = new Student("Frank", "Tran", "frank@gmail.com", 21);
-      studentRepository.save(frank);
+      Student henry = new Student("Henry", "Tran", "henry@gmail.com", 34);
+
+      System.out.println("Adding frank and henry");
+      studentRepository.saveAll(Arrays.asList(frank, henry));
+
+      System.out.print("Number of students: ");
+      studentRepository.count();
+
+      studentRepository.findById(1L).ifPresent(System.out::println);
+
+      System.out.println("Select all students");
+      studentRepository
+          .findAll()
+          .forEach(System.out::println);
+
+      System.out.println("Delete henry");
+      studentRepository.deleteById(2L);
+
+      System.out.print("Number of students:");
+      studentRepository.count();
     };
   }
 
