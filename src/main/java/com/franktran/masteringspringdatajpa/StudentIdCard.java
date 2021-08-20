5871e916d4379be14fc20f6b858e8766ade2c2c3
frank <franktranvantu@gmail.com>
2021-08-21 00:54:45 +0700

    mastering-spring-data-jpa-47 @OneToOne and @JoinColoum

--- src/main/java/com/franktran/masteringspringdatajpa/StudentIdCard.java
+++ src/main/java/com/franktran/masteringspringdatajpa/StudentIdCard.java
@@ -3,16 +3,24 @@ package com.franktran.masteringspringdatajpa;
 import javax.persistence.*;
 
 @Entity
-
+@Table(
+    name = "student_id_card",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "student_id_card_number_unique",
+            columnNames = "card_number"
+        )
+    }
+)
 public class StudentIdCard {
 
   @Id
   @SequenceGenerator(
-      name = "student_id_card",
-      sequenceName = "student_id_card",
+      name = "student_id_card_sequence",
+      sequenceName = "student_id_card_sequence",
       allocationSize = 1
   )
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_id_card")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_id_card_sequence")
   @Column(
       name = "id",
       updatable = false
