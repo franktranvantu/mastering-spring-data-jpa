3f5ec6c9bbf5c6209754fd844a79936e8bbb3f60
frank <franktranvantu@gmail.com>
2021-08-21 01:05:59 +0700

    mastering-spring-data-jpa-50 Student Id Card Repository

---
+++ src/main/java/com/franktran/masteringspringdatajpa/StudentIdCardRepository.java
@@ -0,0 +1,6 @@
+package com.franktran.masteringspringdatajpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentIdCardRepository extends CrudRepository<StudentIdCard, Long> {
+}
