bc04abc0ec115aaf413d1a38493848b1a1c9daf0
frank <franktranvantu@gmail.com>
2021-08-20 22:09:09 +0700

    mastering-spring-data-jpa-18 StudentRepository

--- src/main/java/com/franktran/masteringspringdatajpa/MasteringSpringDataJpaApplication.java
+++ src/main/java/com/franktran/masteringspringdatajpa/MasteringSpringDataJpaApplication.java
@@ -1,13 +1,23 @@
 package com.franktran.masteringspringdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MasteringSpringDataJpaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MasteringSpringDataJpaApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+    return args -> {
+      Student frank = new Student("Frank", "Tran", "frank@gmail.com", 21);
+      studentRepository.save(frank);
+    };
   }
 
 }
