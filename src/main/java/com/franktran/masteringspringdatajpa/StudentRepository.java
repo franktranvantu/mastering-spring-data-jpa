23f913e7557ea9af181fa592df6c1d4f52718860
frank <franktranvantu@gmail.com>
2021-08-20 23:29:15 +0700

    mastering-spring-data-jpa-31 Named Parameters

--- src/main/java/com/franktran/masteringspringdatajpa/StudentRepository.java
+++ src/main/java/com/franktran/masteringspringdatajpa/StudentRepository.java
@@ -2,16 +2,20 @@ package com.franktran.masteringspringdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-  @Query(value = "SELECT * FROM student WHERE email = ?1", nativeQuery = true)
-  Optional<Student> findStudentByEmail(String email);
+  @Query(value = "SELECT s FROM Student s WHERE s.email = :email")
+  Optional<Student> findStudentByEmail(@Param("email") String email);
 
-  @Query(value = "SELECT * FROM Student WHERE first_name = ?1 AND age >= ?2", nativeQuery = true)
-  List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThanEqual(String firstName, Integer age);
+  @Query(value = "SELECT * FROM Student WHERE first_name = :firstName AND age >= :age", nativeQuery = true)
+  List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThanEqual(
+      @Param("firstName") String firstName,
+      @Param("age") Integer age
+  );
 
 }
